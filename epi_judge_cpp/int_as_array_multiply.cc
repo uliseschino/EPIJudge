cacb8a46247dfddb45c4cbe0027af71ac20d57cb
cyb3r14 <74944775+cyb3r14@users.noreply.github.com>
2024-07-22 13:46:44 -0400

    C++ 5.3 int_as_array_multiply

--- epi_judge_cpp/int_as_array_multiply.cc
+++ epi_judge_cpp/int_as_array_multiply.cc
@@ -3,8 +3,30 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> Multiply(vector<int> num1, vector<int> num2) {
-  // TODO - you fill in here.
-  return {};
+  int sign = (num1.front() < 0) ^ (num2.front() < 0) ? -1 : 1;
+  num1.front() = abs(num1.front());
+  num2.front() = abs(num2.front());
+
+  vector<int> result(num1.size() + num2.size(), 0);
+
+  for(int i=num1.size()-1; i >= 0; i--) {
+    for(int j=num2.size()-1; j >= 0; j--) {
+      result[i+j+1] += num1[i] * num2[j];
+      result[i+j] += result[i+j+1] / 10;
+      result[i+j+1] = result[i+j+1] % 10;
+    }
+  }
+
+  result = {find_if_not(begin(result), end(result), [](int a) { return a==0; }), end(result)};
+
+  if(result.empty()) {
+    return {0};
+  }
+
+  result.front() *= sign;
+
+  return result;
+
 }
 
 int main(int argc, char* argv[]) {
